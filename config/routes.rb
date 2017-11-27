0e351880bf02f8e59b30851850293ac4b97df6b8
cgregs32 <cgregs32@gmail.com>
2017-11-26 19:14:10 -0700

    add devise and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-resources :posts do
-  resources :comments
-end
+  devise_for :users
+  resources :posts do
+    resources :comments
+  end
 
-root "posts#index"
+  root "posts#index"
 
-get '/about', to: 'pages#about'
+  get '/about', to: 'pages#about'
 end
