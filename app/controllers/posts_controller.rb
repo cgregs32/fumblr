89b61f05140760c06f57768e618b3abe3d115181
cgregs32 <cgregs32@gmail.com>
2017-11-26 18:02:54 -0700

    delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,42 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @posts = Post.all.order('created_at DESC')
   end
 
   def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
-    @post.save
 
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
@@ -23,4 +44,7 @@ class PostsController < ApplicationController
       params.require(:post).permit(:title, :body)
     end
 
+    def find_post
+      @post = Post.find(params[:id])
+    end
 end
